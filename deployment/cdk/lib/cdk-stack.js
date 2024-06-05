d4d460802c897b49d91ab3320130dc1a343a79c0
Lilac Kcojer <lilackcojer@gmail.com>
2024-06-04 20:59:45 -0400

    updated project structure

--- deployment/cdk/lib/cdk-stack.js
+++ deployment/cdk/lib/cdk-stack.js
@@ -1,7 +1,4 @@
 const cdk = require('aws-cdk-lib');
-const sns = require('aws-cdk-lib/aws-sns');
-const subs = require('aws-cdk-lib/aws-sns-subscriptions');
-const sqs = require('aws-cdk-lib/aws-sqs');
 const dynamodb = require('aws-cdk-lib/aws-dynamodb');
 
 class CdkStack extends cdk.Stack {
@@ -13,17 +10,11 @@ class CdkStack extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
 
     const userTable = new dynamodb.TableV2(this, 'UserTable', {
-      partitionKey: { name: 'username', type: dynamodb.AttributeType.STRING},
+      partitionKey: { name: 'email', type: dynamodb.AttributeType.STRING},
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "userTable"
     });
   }
 }
